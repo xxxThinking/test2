abd22d18972187902888eb5797d8e04734972f22
xxxThinking <973986153@qq.com>
2018-09-29 10:21:41 +0800

    Initial Commit

---
+++ test2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test2
+//
+//  Created by s20171105405 on 2018/9/29.
+//  Copyright © 2018年 s20171105405. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
