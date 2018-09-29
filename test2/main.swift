3bfe5b6609b05691b8e39525481375278b1666c6
xxxThinking <973986153@qq.com>
2018-09-29 10:22:53 +0800

    排序
    简单排序

--- test2/main.swift
+++ test2/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+var index = 0
+var j = 0
+var k = 0
+var  array = [66,13,51,76,81,26,57,69,23,22]
+
+for index in 0...9
+{
+    for j in 0...9
+    {
+        if(array[index]<array[j])
+        {
+            k = array[j]
+            array[j] = array[index]
+            array[index] = k
+        }
+    }
+}
+for i in 0...9
+{
+    print(array[i])
+}
+
 
